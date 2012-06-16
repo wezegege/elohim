37755e588b37e9e644b228542740cedbe95dea89
Wezegege <wezegege@live.fr>
2012-06-16 22:16:08 +0200

    new style hud

--- hud/__init__.py
+++ hud/__init__.py
@@ -1,40 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from PySFMl import sf
 import logging
 
-def entity_list(name, bases, attrs):
-  attrs['logger'] = logging.getLogger(name)
-  result = type(name, bases, attrs)
-  result.items[name.lower()] = result
-  return result
+class Window(object):
+  logger = logging.getLogger('Window')
 
-EntityList = entity_list('EntityList', (object,), dict())
-
-class Entity(EntityList):
-  entity_type = ('parameters', 'controllers', 'views', 'children')
-  items = dict()
-
-  def __init__(self, *args, **kwargs):
-    pass
-
-  def require(self):
-    return list()
-
-  def info(self):
-    result = {
-        'type' : self.__class__.__name__,
-        }
-    for item in self.entity_types:
-      result[item] = [entity.info() for entity in getattr(self, item)]
-    return result
-
-  @classmethod
-  def from_info(cls, info):
-    entity = self.items.get(info.get('type', '').lower(), cls)
-    result = entity()
-    for item in self.entity_types:
-      if item in info:
-        setattr(cls, item, [cls.from_info(sample) for sample in info[item])
-    return result
+  def __init__(self, **kwargs):
+    self.window = None
+    for param, default in (
+        ('widgets',  dict()),
+        ('framerate', 60),
+        ('title', 'Pokertemplate'),
+        ('vsync', True),
+        ):
+      setattr(self, param, kwargs.get(param, default))
 
