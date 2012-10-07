0bb42ae7abcef719ae4e7978ae7b38cf816abfaa
Wezegege <wezegege@live.fr>
2012-10-07 22:13:31 +0200

    some thoughts

--- elohim/gamelib/input/actions.py
+++ elohim/gamelib/input/actions.py
@@ -2,10 +2,11 @@
 # -*- coding: utf-8 -*-
 
 from elohim.engine.mulungu.types import Action, Condition, List, Variable
-from elohim.engine.mulungu import action, param
+from elohim.engine.mulungu import action, param, setting
 
 @param('variable', Variable())
 @param('type', Type())
+@setting('timeout', Integer())
 class Ask(Action):
   name = 'ask'
 
