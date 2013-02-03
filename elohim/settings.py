182182b0d078496d60806fd8b3b476b7284fdf26
Wezegege <wezegege@live.fr>
2013-02-03 23:13:35 +0100

    settings features

---
+++ elohim/settings.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os.path
+
+ROOTPATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+DATAPATH = os.path.join(ROOTPATH, 'resources')
