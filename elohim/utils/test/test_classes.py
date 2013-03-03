421727cdf9f14148a939cd0f2a73ec9f5f300782
Wezegege <wezegege@live.fr>
2013-03-03 20:38:35 +0100

    passing test

--- elohim/utils/test/test_classes.py
+++ elohim/utils/test/test_classes.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from elohim.utils import classes
+
 import os.path
 import unittest2
+
 
 class BaseClass(object):
     pass
@@ -10,8 +13,9 @@ class BaseClass(object):
 
 class TestClasses(unittest2.TestCase):
     def test_list_subclasses(self):
-        subclasses = list_subclasses(BaseClass, __name__, __path__)
-        class_names = (subclass.__name__ for subclass in subclasses)
-        expected = ('SubclassA', 'SubclassB', 'SubclassC')
-        self.assertSequenceEqual(expected, class_names)
+        path = os.path.dirname(__file__)
+        subclasses = classes.list_subclasses(BaseClass, __name__, path)
+        class_names = [subclass.__name__ for subclass in subclasses]
+        for expected in ('SubclassA', 'SubclassB', 'SubclassC'):
+            self.assertIn(expected, class_names)
 
