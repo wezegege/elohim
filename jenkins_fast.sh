a3efd547d67ddf94c9a7a9e03508d421450c2547
Wezegege <wezegege@live.fr>
2013-03-04 13:34:09 +0100

    fast jenkins script

---
+++ jenkins_fast.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -xe
+
+if [ $# -lt 2 ]; then
+    echo 'Usage : jenkins_tests.sh <workspace> <python version>'
+    exit 1
+fi
+
+WORKSPACE=$1
+version=$2
+PYENV_HOME=${WORKSPACE}/virtualenv
+
+if [ -d ${PYENV_HOME} ]; then
+    source ${PYENV_HOME}/bin/activate
+    pip install --quiet --upgrade -r ${WORKSPACE}/requirements.txt
+else
+    virtualenv --no-site-packages -p python${version} ${PYENV_HOME}
+    source ${PYENV_HOME}/bin/activate
+    pip install --quiet -r ${WORKSPACE}/requirements.txt
+    case "${version}" in
+        2*) libraries='nosexcover unittest2';;
+        3*) libraries='unittest2py3k git+https://github.com/cmheisel/nose-xcover.git';;
+    esac
+    pip install --quiet nose pylint mock ${libraries}
+    python ${WORKSPACE}/setup.py develop
+fi
+
+pylint -f parseable elohim | tee pylint.out
+nosetests --with-xcoverage --with-xunit --cover-package=elohim --cover-erase --with-doctest
+deactivate
